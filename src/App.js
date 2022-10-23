1f2ef77f88bf33971a65cd55f38428025a61b9a1
tsotne123 <tsotne@profairgames.com>
2022-10-23 14:32:43 +0400

    add homepage and bacgkround effect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import HomePage from './components/pages/homePage';
 
 function App() {
   return (
     <div className="App">
-  
+      <HomePage />
     </div>
   );
 }
---
+++ src/components/backgroundEffect/index.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import "./style.css"
+
+import music_note from "../../img/music_note.png"
+
+const Backgroundeffect = () => {
+
+
+
+
+  return (
+    <div className='background-effect'>
+            <img style={{animationDelay:"0s",left:"0px", scale : "0.7" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"0.2s",left:"1400px", scale : "0.5" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"0.4s",left:"800px", scale : "0.6" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"0.6s",left:"1600px", scale : "0.3" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"0.8s",left:"900px", scale : "0.8" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"1s",left:"1200px", scale : "0.7" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"1.2s",left:"600px", scale : "0.5" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"1.4s",left:"300px", scale : "0.9" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"1.6s",left:"1500px", scale : "0.6" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"1.8s",left:"400px", scale : "0.3" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"2s",left:"900px", scale : "0.6" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"2.2s",left:"100px", scale : "0.8" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"2.4s",left:"1600px", scale : "0.9" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"2.6s",left:"1000px", scale : "0.6" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"2.8s",left:"1500px", scale : "0.5" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"3s",left:"500px", scale : "0.7" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"3.2s",left:"1700px", scale : "0.9" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"3.4s",left:"300px", scale : "0.5" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"3.6s",left:"900px", scale : "0.3" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"3.8s",left:"1600px", scale : "0.7" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"4s",left:"1100px", scale : "0.4" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"4.2s",left:"0px", scale : "0.6" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"4.4s",left:"1720px", scale : "0.4" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"4.6s",left:"1320px", scale : "0.8" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"4.8s",left:"620px", scale : "0.5" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"5s",left:"20px", scale : "0.9" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"5.2s",left:"100px", scale : "0.4" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"5.4s",left:"1600px", scale : "0.7" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"5.6s",left:"500px", scale : "0.5" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"5.8s",left:"0px", scale : "0.3" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"6s",left:"1000px", scale : "0.6" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"6.2s",left:"700px", scale : "0.5" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"6.4s",left:"1720px", scale : "0.4" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"6.6s",left:"1120px", scale : "0.8" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"6.8s",left:"320px", scale : "0.5" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"7s",left:"1620px", scale : "0.7" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"7.2s",left:"920px", scale : "0.5" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"7.4s",left:"1200px", scale : "0.6" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"7.6s",left:"370px", scale : "0.4" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"7.8s",left:"1370px", scale : "0.8" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"8s",left:"900px", scale : "0.5" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"8.2s",left:"1720px", scale : "0.6" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"8.4s",left:"80px", scale : "0.5" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"8.6s",left:"680px", scale : "0.8" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"8.8s",left:"1680px", scale : "0.4" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"9s",left:"1380px", scale : "0.6" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"9.2s",left:"980px", scale : "0.7" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"9.4s",left:"1500px", scale : "0.3" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"9.6s",left:"100px", scale : "0.5" }} className='background-effect-img' src={music_note} alt="" />
+            <img style={{animationDelay:"9.8s",left:"1000px", scale : "0.3" }} className='background-effect-img' src={music_note} alt="" />
+    </div>
+  )
+}
+
+export default Backgroundeffect
