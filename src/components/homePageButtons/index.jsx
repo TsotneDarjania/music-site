5b49039a2354ec14f59ee0b62749adab2c8fa422
tsotne123 <tsotne@profairgames.com>
2022-11-25 13:34:28 +0400

    update code by framer-motion

--- src/components/homePageButtons/index.jsx
+++ src/components/homePageButtons/index.jsx
@@ -4,6 +4,9 @@ import { forwardRef } from 'react';
 import { useState } from 'react'
 import "./style.css"
 
+import {motion} from "framer-motion"
+
+
 const HomePagebuttons =  forwardRef ( (props,ref) => {
 
     useImperativeHandle( ref, () => {
@@ -17,66 +20,162 @@ const HomePagebuttons =  forwardRef ( (props,ref) => {
     const rightButtonRef = useRef();
     const bottomButtonRef = useRef();
 
-    const [animateTopButton, setAnimateTopButton] = useState("")
-    const [animateLeftButton, setAnimateLeftButton] = useState("")
-    const [animateBottomButton, setAnimateBottomButton] = useState("")
-    const [animateRightButton, setAnimateRightButton] = useState("")
+    const [extraClassTopButton,setextraClassTopButton] = useState("");
+    const [extraClassBottomButton,setextraClassBottomButton] = useState("");
+    const [extraClassLeftButton,setextraClassLeftButton] = useState("");
+    const [extraClassRightButton,setextraClassRIghtButton] = useState("");
 
+ 
 
 
     useEffect(() => {
-        topButtonRef.current.addEventListener('animationend', (anim) => {
-            if (anim.animationName === "hide-option-btn") {
-                setAnimateTopButton("display-none")
-            }
-        });
-        leftButtonRef.current.addEventListener('animationend', (anim) => {
-            if (anim.animationName === "hide-option-btn") {
-                setAnimateLeftButton("display-none")
-            }
-        });
-        rightButtonRef.current.addEventListener('animationend', (anim) => {
-            if (anim.animationName === "hide-option-btn") {
-                setAnimateRightButton("display-none")
-            }
-        });
-        bottomButtonRef.current.addEventListener('animationend', (anim) => {
-            if (anim.animationName === "hide-option-btn") {
-                setAnimateBottomButton("display-none")
-            }
-        });
+        
     },[])
 
 
     const selectOption = (option) => {
-        setAnimateTopButton("animate-top-button")
-        setAnimateLeftButton("hide-option-btn")
-        setTimeout(() => {
-            setAnimateBottomButton("hide-option-btn")
-        },400);
-        setTimeout(() => {
-            setAnimateRightButton("hide-option-btn")
-            props.webIconClass("web-icon-to-right-anim")
-            props.newPageMode("quiz");
-        },800);
+       props.changeMode(option)
     }
 
     function backToHome(){
-        topButtonRef.current.style.animationDirection = "alternate-reverse";
-        topButtonRef.current.style.animationName = "top-button-anim-reverse";
 
-        setAnimateRightButton(" ")
-        setAnimateLeftButton(" ")
-        setAnimateBottomButton(" ")
+        const animations = [topButtonAnimation,leftButtonAnimation,rightButtonAnimation,bottomButtonAnimation]
+        const setAnimations = [setTopButtonAnimation,setLeftButtonAnimation,setRightButtonAnimation,setBottomButtonAnimation]
+
+        for (let i = 0; i < 4; i++) {
+
+            if(animations[i] === "disabled" || "hide"){ setAnimations[i]("init") } 
+            else { setAnimations[i]("reverseStretching") }
+            
+        }
     }
+
+    const topButtonAnimations = {
+        init: {display:"block", opacity: 0.4},
+        stretching: {display:"block",width:500, opacity:1},
+        expand: {display:"block",width:500, opacity:1,height:80,fontSize:"25px",color:"aqua",borderBottom:"1px solid aqua"},
+        toTop: {display:"block",width:500, opacity:1,height:80,fontSize:"25px",color:"aqua",background:"none",borderBottom:"1px solid aqua",top:"-87vh"},
+
+        reverseStretching: {display:"block",width:300, opacity:1,height:80,fontSize:"20px",color:"aqua",background:"none",borderBottom:"1px solid aqua",top:"-87vh"},
+        reverseExpand: {display:"block",width:300, opacity:1,height:60,top:"-87vh"},
+        reverseToTop:{display:"block", opacity:0.4}
+    }
+    const [topButtonAnimation,setTopButtonAnimation] = useState("init");
+
+    const leftButtonAnimations = {
+        init: {display:"block", opacity: 0.4},
+        hide: {display:"block",opacity: 0},
+        disable:{display:"none"}
+    }
+    const [leftButtonAnimation,setLeftButtonAnimation] = useState("init");
+
+    const bottomButtonAnimations = {
+        init: {display:"block", opacity: 0.4},
+        hide: {display:"block",opacity: 0},
+        disable:{display:"none"}
+    }
+    const [bottomButtonAnimation,setBottomButtonAnimation] = useState("init");
+
+    const rightButtonAnimations = {
+        init: {display:"block", opacity: 0.4},
+        hide: {display:"block",opacity: 0},
+        disable:{display:"none"}
+    }
+    const [rightButtonAnimation,setRightButtonAnimation] = useState("init");
 
   return (
     <div>
-        {/* onAnimationEnd={} */}
-        <button  ref={topButtonRef} onClick={ () => {selectOption("quiz")}} style={{animationDelay : "1s"}} className={'select-option-btn center select-option-btn-top ' + animateTopButton}> მუსიკალური ქვიზები </button>
-        <button ref={leftButtonRef} onClick={ () => {selectOption("social")}} style={{animationDelay : "1.5s"}} className={'select-option-btn center select-option-btn-left ' + animateLeftButton}> სოციალური ქსელი </button>
-        <button ref={rightButtonRef} onClick={ () => {selectOption("about")}} style={{animationDelay : "2s"}} className={'select-option-btn center select-option-btn-right ' + animateRightButton}> ჩემს შესახებ </button>
-        <button ref={bottomButtonRef} onClick={ () => {selectOption("blogs")}} style={{animationDelay : "2.5s"}} className={'select-option-btn center select-option-btn-bottom ' + animateBottomButton}> ბლოგები </button> 
+        <motion.button  
+        type='button'
+            animate = {topButtonAnimation}
+            variants = {topButtonAnimations}
+            ref={topButtonRef} 
+            transition={ { delay: topButtonAnimation === "init" ? 1:0, ease:"anticipate", duration: 0.6 } }
+            onClick={ () => {  
+                setextraClassTopButton("select-option-none-transition") 
+
+                setTopButtonAnimation("expand")
+                setLeftButtonAnimation("hide")
+                setRightButtonAnimation("hide")
+                setBottomButtonAnimation("hide")
+
+                selectOption("quiz")
+
+            }} 
+            onAnimationComplete={ (anim) => {
+                if(anim === "stretching"){
+                    setTopButtonAnimation("expand") 
+                }
+                if(anim === "expand"){
+                    setTopButtonAnimation("toTop") 
+                }
+
+                if(anim === "reverseStretching"){
+                    setTopButtonAnimation("reverseExpand") 
+                }
+                if(anim === "reverseExpand"){
+                    setTopButtonAnimation("reverseToTop") 
+                }
+            }}
+            className={'select-option-btn center select-option-btn-top '+extraClassTopButton}>
+            მუსიკალური ქვიზები 
+        </motion.button>
+
+        <motion.button  
+        type='button'
+            animate = {leftButtonAnimation}
+            variants = {leftButtonAnimations}
+            ref={leftButtonRef} 
+            transition={ { delay: leftButtonAnimation === "init" ? 1.5:0, type: 'spring',duration: leftButtonAnimation === "hide" ? 0.2:2 } }
+            onClick={ () => {  
+             
+            }} 
+            onAnimationComplete={ (anim) => {
+                if(anim === "hide"){
+                    setLeftButtonAnimation("disabled")  
+                }
+            }}
+            className={'select-option-btn center select-option-btn-left '+extraClassLeftButton}>
+            სოციალური ქსელი
+        </motion.button>
+
+        <motion.button  
+        type='button'
+            animate = {bottomButtonAnimation}
+            variants = {bottomButtonAnimations}
+            ref={bottomButtonRef} 
+            transition={ { delay: bottomButtonAnimation === "init" ? 2:0, type: 'spring',duration: bottomButtonAnimation === "hide" ? 0.2:2 } }
+            onClick={ () => {  
+             
+            }} 
+            onAnimationComplete={ (anim) => {
+                if(anim === "hide"){
+                    setBottomButtonAnimation("disabled")  
+                }
+            }}
+            className={'select-option-btn center select-option-btn-bottom '+extraClassBottomButton}>
+            ბლოგები
+        </motion.button>
+
+        <motion.button  
+        type='button'
+            animate = {rightButtonAnimation}
+            variants = {rightButtonAnimations}
+            ref={rightButtonRef} 
+            transition={ {delay: rightButtonAnimation === "init" ? 2.5:0, type: 'spring',duration: rightButtonAnimation === "hide" ? 0.2:2 } }
+            onClick={ () => {  
+             
+            }} 
+            onAnimationComplete={ (anim) => {
+                if(anim === "hide"){
+                    setRightButtonAnimation("disabled")  
+                }
+            }}
+            className={'select-option-btn center select-option-btn-right '+extraClassRightButton}>
+            ჩემს შესახებ
+        </motion.button>
+       
+      
     </div>
   )
 })
--- src/components/pages/homePage/index.jsx
+++ src/components/pages/homePage/index.jsx
@@ -7,47 +7,104 @@ import Backgroundeffect from '../../backgroundEffect'
 import HomePagebuttons from '../../homePageButtons'
 import { useState } from 'react'
 import { useRef } from 'react'
+import { motion} from "framer-motion"
+
 
 const HomePage = () => {
 
-  const webIconReferenc = useRef();
   const childRef = useRef();
 
   const[mainPageMode, setMainPageMode] = useState("intro")
-  const[newPageMode, setNewPageMode] = useState("")
 
-  const [webIconClass,setWebIconClass] = useState("")
   const [backgroundEffectClass,setbackgroundEffectClass] = useState("")
 
+  const webIconAnimations = {
+    init : {opacity:1, scale:1},
+    toRight : {opacity:1, scale:1,backgroundColor:"rgb(1, 11, 22)",
+    border:"2px", right:"-90vw"},
+    toTop : {opacity:1, scale:1,backgroundColor:"rgb(1, 11, 22)",
+    border:"2px", right:"-92vw",top:"-85vh"},
+
+    reverseToTop:{opacity:1, scale:1,backgroundColor:"rgb(1, 11, 22)",
+    border:"2px", right:"-90vw"},
+    reverseToRight:{opacity:1, scale:1}
+  }
+  const [webIconAnimation,setWebIconAnimation] = useState("init");
+
+  const closeButtonAnimations = {
+    init : {opacity:1},
+    hide : {opacity:0,display:"none"}
+  }
+
+  const [closeButtonAnimation,setCloseButtonAnimation] = useState("hide");
+
+
   useEffect(() => {
-    webIconReferenc.current.addEventListener('animationend', (anim) => { 
-      anim.animationName === "web-icon-to-right-anim" && ChangeMode(newPageMode)
-    });
+  
   })
 
   function backToIntro(){
-    setNewPageMode("intro")
-    document.querySelector(".close-button").style.display = "none"
+    setMainPageMode("intro")
+
+    setWebIconAnimation("reverseToTop")
 
     childRef.current.backToHome();
-
-    webIconReferenc.current.style.animationDirection = "alternate-reverse";
-    webIconReferenc.current.style.animationName = "web-icon-to-right-anim-reverse";
   }
 
-  function ChangeMode(mode){
+  const changeMode = (mode) => {  
     setMainPageMode(mode);
+
+    setWebIconAnimation("toRight")
     setbackgroundEffectClass("hide-anim")
+  
   }
 
   return (
     <div className='home-page'>
         {
-          mainPageMode !== "intro" && <img onClick={backToIntro} className='close-button' src={closeButton} alt="close-button"   />
+          mainPageMode !== "intro" && 
+          <motion.img 
+            animate = {closeButtonAnimation}
+            variants = {closeButtonAnimations}
+            transition={ {duration:0.8 } }
+            onClick={backToIntro} 
+            className='close-button' 
+            src={closeButton} 
+            alt="close-button"   
+          />
         }
         <div className={backgroundEffectClass}> <Backgroundeffect /> </div> 
-        <img ref={webIconReferenc} onClick={ () => {window.open("https://www.youtube.com/watch?v=rOjHhS5MtvA")}} className={'web-icon center ' + webIconClass} src={icon} alt="web-icon" />
-        <HomePagebuttons ref={childRef} newPageMode={setNewPageMode} webIconClass = {setWebIconClass} />
+        <motion.img 
+          animate = {webIconAnimation}
+          variants = {webIconAnimations}
+          transition={ {delay: webIconAnimation === "init" ? 0.5 : 0.3, duration: 0.2 } }
+          onAnimationComplete={ (anim)=> {
+            if(anim === "toRight"){
+              setWebIconAnimation("toTop")
+            }
+            if(anim === "reverseToRight"){
+              setTimeout(() => {
+                setbackgroundEffectClass("show-anim")
+              }, 500);
+            }
+            if(anim === "reverseToTop"){
+              setWebIconAnimation("reverseToRight")
+            }
+            
+
+            if(anim === "toTop"){
+              setCloseButtonAnimation("init")
+            }
+          }}
+          onClick={ () => {window.open("https://www.youtube.com/watch?v=rOjHhS5MtvA")}} 
+          className={'web-icon center '} src={icon} alt="web-icon"
+         />
+        <HomePagebuttons 
+          ref={childRef}
+          changeMode = {(mode) => {
+              changeMode(mode);
+          }} 
+        />
     </div>
   )
 }
